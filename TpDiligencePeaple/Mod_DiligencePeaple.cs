b06cffbf3963b9d077f6e9e7cb68968da988bf7d
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-13 01:21:21 +0900

    TpDiligencePeaple

---
+++ TpDiligencePeaple/Mod_DiligencePeaple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+
+namespace TpDiligencePeaple
+{
+	[BepInPlugin("net.kuronekotei.diligence_peaple", "DiligencePeaple", "1.0.0.0")]
+	public class Mod_DiligencePeaple : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("DiligencePeaple").PatchAll();
+		}
+	}
+}
