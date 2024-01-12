9b394bd8d09006d09090c86c0c63d4138e64cc72
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-13 00:51:55 +0900

    TpHalloMod

---
+++ TpHalloMod/HalloMod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BepInEx;
+using HarmonyLib;
+
+using ReflexCLI.Attributes;
+
+using UnityEngine;
+
+namespace TpHalloMod
+{
+	[HarmonyPatch]
+	public class HalloMod
+	{
+		[HarmonyPrefix, HarmonyPatch(typeof(CoreDebug), nameof(CoreDebug.GodMode))]
+		public static void GodMode() {
+			Debug.Log("HalloMod");
+			if (EClass.debug.godMode) {
+				EClass.debug.godMode = false;
+				EClass.debug._godBuild = false;
+				EClass.debug.godCraft = true;
+			} else {
+				EClass.debug.godMode = true;
+				EClass.debug._godBuild = true;
+				EClass.debug.godCraft = true;
+			}
+		}
+		[HarmonyPrefix, HarmonyPatch(typeof(CoreDebug), nameof(CoreDebug.FlyMode))]
+		public static void FlyMode() {
+			Debug.Log("HalloMod");
+			if (EClass.debug.showExtra) {
+				EClass.debug.inviteAnytime = false;
+				EClass.debug.showExtra = false;
+			} else {
+				EClass.debug.inviteAnytime = true;
+				EClass.debug.showExtra = true;
+			}
+		}
+	}
+}
---
+++ TpHalloMod/Mod_HalloMod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BepInEx;
+using HarmonyLib;
+
+using ReflexCLI.Attributes;
+
+namespace TpHalloMod
+{
+	[BepInPlugin("net.kuronekotei.hallomod", "HalloMod", "1.0.0.0")]
+	public class Mod_HalloMod : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("HalloMod").PatchAll();
+		}
+	}
+}
