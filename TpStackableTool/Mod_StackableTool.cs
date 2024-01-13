3bd2b1a54a4d4ca256a4eb221e83ac63c6e17cdc
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-14 01:34:51 +0900

    TpStackableTool

---
+++ TpStackableTool/Mod_StackableTool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpStackableTool
+{
+	[BepInPlugin("net.kuronekotei.stackable_tool", "StackableTool", "1.0.0.0")]
+	public class Mod_StackableTool : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("StackableTool").PatchAll();
+		}
+	}
+}
