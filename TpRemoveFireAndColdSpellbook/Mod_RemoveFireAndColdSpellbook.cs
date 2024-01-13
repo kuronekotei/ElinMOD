c4bba167589f5124ddb5580105cddb304731e21e
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-14 00:47:46 +0900

    TpRemoveFireAndColdSpellbook

---
+++ TpRemoveFireAndColdSpellbook/Mod_RemoveFireAndColdSpellbook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpRemoveFireAndColdSpellbook
+{
+	[BepInPlugin("net.kuronekotei.remove_fire_and_cold_spellbook", "RemoveFireAndColdSpellbook", "1.0.0.0")]
+	public class Mod_RemoveFireAndColdSpellbook : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("RemoveFireAndColdSpellbook").PatchAll();
+		}
+	}
+}
