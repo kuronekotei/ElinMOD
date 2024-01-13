26b985ac9e814dcf8d71f18fe1e647c457e08734
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-13 19:29:08 +0900

    TpHugeTrashContainer

---
+++ TpHugeTrashContainer/HugeTrashContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using UnityEngine;
+
+namespace TpHugeTrashContainer
+{
+	[HarmonyPatch]
+	public class HugeTrashContainer
+    {
+	}
+}
---
+++ TpHugeTrashContainer/Mod_HugeTrashContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpHugeTrashContainer
+{
+	[BepInPlugin("net.kuronekotei.huge_trash_container", "HugeTrashContainer", "1.0.0.0")]
+	public class Mod_HugeTrashContainer : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("HugeTrashContainer").PatchAll();
+		}
+	}
+}
