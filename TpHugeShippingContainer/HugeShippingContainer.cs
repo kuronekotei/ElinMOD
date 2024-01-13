6fcc6289de65699caac908cc64f99ee9ad470b3f
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-13 17:04:08 +0900

    TpHugeShippingContainer

---
+++ TpHugeShippingContainer/HugeShippingContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using UnityEngine;
+
+namespace TpHugeShippingContainer
+{
+	[HarmonyPatch]
+	public class HugeShippingContainer
+    {
+	}
+}
---
+++ TpHugeShippingContainer/Mod_HugeShippingContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpHugeShippingContainer
+{
+	[BepInPlugin("net.kuronekotei.huge_shipping_container", "HugeShippingContainer", "1.0.0.0")]
+	public class Mod_HugeShippingContainer : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("HugeShippingContainer").PatchAll();
+		}
+	}
+}
