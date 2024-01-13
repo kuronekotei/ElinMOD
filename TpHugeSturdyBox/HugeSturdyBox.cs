59c807083fb4a06788207bd4751c5bcb4e05f7d1
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-13 20:54:17 +0900

    TpHugeSturdyBox

---
+++ TpHugeSturdyBox/HugeSturdyBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using UnityEngine;
+
+namespace TpHugeSturdyBox
+{
+    public class HugeSturdyBox
+    {
+	}
+}
---
+++ TpHugeSturdyBox/Mod_HugeSturdyBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpHugeSturdyBox
+{
+	[BepInPlugin("net.kuronekotei.huge_sturdy_box", "HugeSturdyBox", "1.0.0.0")]
+	public class Mod_HugeSturdyBox : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("HugeSturdyBox").PatchAll();
+		}
+	}
+}
