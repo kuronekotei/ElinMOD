cdaaaf81186109c3d13d6388c3f62783381fb918
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-14 01:23:06 +0900

    TpStackableSpellbook

---
+++ TpStackableSpellbook/Mod_StackableSpellbook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpStackableSpellbook
+{
+	[BepInPlugin("net.kuronekotei.stackable_spellbook", "StackableSpellbook", "1.0.0.0")]
+	public class Mod_StackableSpellbook : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("StackableSpellbook").PatchAll();
+		}
+	}
+}
