407e834bfe0e753f0f1747a93c98cf8e8b6478d2
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-14 00:32:39 +0900

    TpHugeFridge

---
+++ TpHugeFridge/Mod_HugeFridge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpHugeFridge
+{
+	[BepInPlugin("net.kuronekotei.huge_fridge", "HugeFridge", "1.0.0.0")]
+	public class Mod_HugeFridge : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("HugeFridge").PatchAll();
+		}
+	}
+}
