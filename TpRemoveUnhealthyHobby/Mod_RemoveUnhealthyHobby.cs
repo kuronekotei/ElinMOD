0dfaeb05c01d534994678a76a263f504dab57145
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-13 15:54:51 +0900

    TpRemoveUnhealthyHobby

---
+++ TpRemoveUnhealthyHobby/Mod_RemoveUnhealthyHobby.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpRemoveUnhealthyHobby
+{
+	[BepInPlugin("net.kuronekotei.remove_unhealthy_hobby", "RemoveUnhealthyHobby", "1.0.0.0")]
+	public class Mod_RemoveUnhealthyHobby : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("RemoveUnhealthyHobby").PatchAll();
+		}
+	}
+}
---
+++ TpRemoveUnhealthyHobby/RemoveUnhealthyHobby.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BepInEx;
+using HarmonyLib;
+
+using ReflexCLI.Attributes;
+
+using UnityEngine;
+
+namespace TpHalloMod
+{
+	[HarmonyPatch]
+	public class RemoveUnhealthyHobby
+	{
+	}
+}
