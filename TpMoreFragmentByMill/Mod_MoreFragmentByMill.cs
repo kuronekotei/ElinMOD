4dc7ffd85e0e53ebccfca6918af9860c9e178ce2
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-15 00:06:00 +0900

    TpMoreFragmentByMill

---
+++ TpMoreFragmentByMill/Mod_MoreFragmentByMill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpMoreFragmentByMill
+{
+	[BepInPlugin("net.kuronekotei.more_fragment_by_mill", "MoreFragmentByMill", "1.0.0.0")]
+	public class Mod_MoreFragmentByMill : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("MoreFragmentByMill").PatchAll();
+		}
+	}
+}
