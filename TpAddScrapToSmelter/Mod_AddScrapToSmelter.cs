a84005b98c9f8bfe3220c71b2c49c32d1c95dbec
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-14 11:08:23 +0900

    TpAddScrapToSmelter

---
+++ TpAddScrapToSmelter/Mod_AddScrapToSmelter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpAddScrapToSmelter
+{
+	[BepInPlugin("net.kuronekotei.add_scrap_to_smelter", "AddScrapToSmelter", "1.0.0.0")]
+	public class Mod_AddScrapToSmelter : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("AddScrapToSmelter").PatchAll();
+		}
+	}
+}
