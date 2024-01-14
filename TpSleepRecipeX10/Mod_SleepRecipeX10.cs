9cdfdf02814e23173693a2635171d0441a1375f1
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-14 10:40:19 +0900

    TpSleepRecipeX10

---
+++ TpSleepRecipeX10/Mod_SleepRecipeX10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpSleepRecipeX10
+{
+	[BepInPlugin("net.kuronekotei.sleep_recipe_x10", "SleepRecipeX10", "1.0.0.0")]
+	public class Mod_SleepRecipeX10 : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("SleepRecipeX10").PatchAll();
+		}
+	}
+}
