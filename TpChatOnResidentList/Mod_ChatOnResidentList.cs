66b2d7ffe3ce38e06ab871199c794505a21b3d42
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-15 00:38:55 +0900

    TpChatOnResidentList

---
+++ TpChatOnResidentList/Mod_ChatOnResidentList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpChatOnResidentList
+{
+	[BepInPlugin("net.kuronekotei.chat_on_resident_list", "ChatOnResidentList", "1.0.0.0")]
+	public class Mod_ChatOnResidentList : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("ChatOnResidentList").PatchAll();
+		}
+	}
+}
