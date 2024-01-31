64193285b8c3cba6b9bbaed222b077ae855c5293
TP@黒猫亭 <tp@kuronekotei.net>
2024-01-31 14:05:25 +0900

    TpLongSightInHome

---
+++ TpLongSightInHome/LongSightInHome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using UnityEngine;
+
+
+namespace TpLongSightInHome
+{
+	[HarmonyPatch]
+	public class LongSightInHome
+	{
+	}
+}
---
+++ TpLongSightInHome/Mod_LongSightInHome.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpLongSightInHome
+{
+	[BepInPlugin("net.kuronekotei.huge_fridge", "LongSightInHome", "1.0.0.0")]
+	public class Mod_LongSightInHome : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("LongSightInHome").PatchAll();
+		}
+	}
+}
