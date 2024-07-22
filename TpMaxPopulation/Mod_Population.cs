fa99f2f9469bcba24a595efaf30aad22fbfbf4c3
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:35:52 +0900

    TpPopulation  ElinBeta

---
+++ TpMaxPopulation/Mod_Population.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpPopulation
+{
+	
+	[BepInPlugin("net.kuronekotei.population", "Population", "1.0.0.0")]
+	public class Mod_Population : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("Population").PatchAll();
+		}
+	}
+}
---
+++ TpMaxPopulation/Population.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using UnityEngine;
+
+namespace TpPopulation
+{
+	[HarmonyPatch]
+	public class Population
+    {
+		[HarmonyPrefix, HarmonyPatch(typeof(FactionBranch), nameof(FactionBranch.MaxPopulation), MethodType.Getter)]
+		public static bool MaxPopulation(FactionBranch __instance, ref int __result) {
+			__result = 100 + __instance.Evalue(2204);
+			return false;
+		}
+	}
+}
