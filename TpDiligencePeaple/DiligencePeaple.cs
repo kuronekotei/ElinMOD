a23ab3f1b4a2a50fdc6898601630115b96825ae4
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:26:21 +0900

    TpDiligencePeaple  ElinBeta

--- TpDiligencePeaple/DiligencePeaple.cs
+++ TpDiligencePeaple/DiligencePeaple.cs
@@ -15,5 +15,15 @@ namespace TpDiligencePeaple
 	[HarmonyPatch]
 	public class DiligencePeaple
 	{
+		[HarmonyPostfix, HarmonyPatch(typeof(Chara), nameof(Chara.RerollHobby))]
+		public static void RerollHobby(Chara __instance) {
+			Debug.Log("DiligencePeaple");
+			if (__instance._works?.Count < 2) {
+				int newWork = EClass.sources.hobbies.listWorks.RandomItem<SourceHobby.Row>().id;
+				newWork = __instance._works[0] != newWork ? newWork : EClass.sources.hobbies.listWorks.RandomItem<SourceHobby.Row>().id;
+				newWork = __instance._works[0] != newWork ? newWork : EClass.sources.hobbies.listWorks.RandomItem<SourceHobby.Row>().id;
+				__instance.AddWork(newWork);
+			}
+		}
 	}
 }
