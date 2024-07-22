d7a68afcb25eb5371f0b1f15f2658751f9d5f404
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:36:40 +0900

    TpNoCapAlly  ElinBeta

---
+++ TpNoCapAlly/Mod_NoCapAlly.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpNoCapAlly
+{
+	[BepInPlugin("net.kuronekotei.no_cap_ally", "NoCapAlly", "1.0.0.0")]
+	public class Mod_NoCapAlly : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("NoCapAlly").PatchAll();
+		}
+	}
+}
---
+++ TpNoCapAlly/NoCapAlly.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using UnityEngine;
+
+namespace TpNoCapAlly
+{
+	[HarmonyPatch]
+	public class NoCapAlly
+    {
+		[HarmonyPrefix, HarmonyPatch(typeof(Player), nameof(Player.MaxAlly), MethodType.Getter)]
+		public static bool MaxAlly(ref int __result) {
+			__result = Mathf.Max(EClass.pc.CHA / 10, 1) + EClass.pc.Evalue(1645);
+			return false;
+		}
+		[HarmonyPrefix, HarmonyPatch(typeof(Player), nameof(Player.RefreshEmptyAlly))]
+		public static bool RefreshEmptyAlly(Player __instance) {
+			int num = __instance.MaxAlly - EClass.pc.party.members.Count + 1;
+			if (__instance.MaxAlly > 8) 
+			{
+				num = 8 * (__instance.MaxAlly - EClass.pc.party.members.Count) / __instance.MaxAlly + 1;
+			}
+			if (num == __instance.lastEmptyAlly) { return false; }
+			__instance.lastEmptyAlly = num;
+			foreach (Chara member in EClass.pc.party.members) {
+				member.RefreshSpeed();
+			}
+			return false;
+		}
+
+	}
+}
