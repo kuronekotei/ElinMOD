930060c00792efd763b047dd4cc3c074d52e0f69
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:30:54 +0900

    TpHugeSturdyBox  ElinBeta

--- TpHugeSturdyBox/HugeSturdyBox.cs
+++ TpHugeSturdyBox/HugeSturdyBox.cs
@@ -13,5 +13,31 @@ namespace TpHugeSturdyBox
 	[HarmonyPatch]
 	public class HugeSturdyBox
     {
+		[HarmonyPrefix, HarmonyPatch(typeof(Game), nameof(Game.OnGameInstantiated))]
+		public static void OnGameInstantiated_Prefix() {
+			List<SourceThing.Row> list = EClass.sources.things.rows;
+			FixContainerSize(list?.Find(x => x.name_JP == "頑丈な箱"), 14, 10);
+		}
+
+		private static void FixContainerSize(SourceThing.Row thing, int w, int h) {
+			var traits = thing?.trait;
+			if (traits == null) {
+				return;
+			}
+			for (int i = 0; i < traits.Count(); i++) {
+				try {
+					if (Regex.Match(traits[i], @"^(Container.*)|(.*Chest)|(Fridge)$").Success
+						&& (i + 2 < traits.Count())
+					) {
+						int wx = int.Parse(traits[i + 1]);
+						int hx = int.Parse(traits[i + 2]);
+						traits[i + 1] = (w).ToString();
+						traits[i + 2] = (h).ToString();
+						i += 2;
+					}
+
+				} catch (Exception) { }
+			}
+		}
 	}
 }
