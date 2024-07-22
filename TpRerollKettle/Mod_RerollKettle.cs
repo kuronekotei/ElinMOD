6df46ac6ab2f42600465e9f9cd060b8ee37855dc
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:37:55 +0900

    TpRerollKettle  ElinBeta

---
+++ TpRerollKettle/Mod_RerollKettle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpRerollKettle
+{
+	[BepInPlugin("net.kuronekotei.reroll_kettle", "RerollKettle", "1.0.0.0")]
+	public class Mod_RerollKettle : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("RerollKettle").PatchAll();
+		}
+	}
+}
---
+++ TpRerollKettle/RerollKettle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using UnityEngine;
+
+namespace TpRerollKettle
+{
+	[HarmonyPatch]
+	public class RerollKettle
+    {
+		[HarmonyPrefix, HarmonyPatch(typeof(TraitKettle), nameof(TraitKettle.CostRerollShop), MethodType.Getter)]
+		public static bool MaxAlly(ref int __result) {
+			__result = 5;
+			return false;
+		}
+		[HarmonyPrefix, HarmonyPatch(typeof(TraitKettle), nameof(TraitKettle.RestockDay), MethodType.Getter)]
+		public static bool RestockDay(ref int __result) {
+			__result = 5;
+			return false;
+		}
+		[HarmonyPrefix, HarmonyPatch(typeof(Trait), nameof(Trait.NumCopyItem), MethodType.Getter)]
+		public static bool NumCopyItem(Trait __instance,ref int __result) {
+			__result = 3 + Mathf.Min(__instance.owner.c_invest / 5, 7);
+			return false;
+		}
+
+	}
+}
