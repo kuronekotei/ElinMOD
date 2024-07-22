7ed541c4de30674559e55f654bb1d1b4159a4098
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:31:11 +0900

    TpHugeTrashContainer  ElinBeta

--- TpHugeTrashContainer/HugeTrashContainer.cs
+++ TpHugeTrashContainer/HugeTrashContainer.cs
@@ -13,5 +13,33 @@ namespace TpHugeTrashContainer
 	[HarmonyPatch]
 	public class HugeTrashContainer
     {
+		[HarmonyPrefix, HarmonyPatch(typeof(Game), nameof(Game.OnGameInstantiated))]
+		public static void OnGameInstantiated_Prefix() {
+			List<SourceThing.Row> list = EClass.sources.things.rows;
+			FixContainerSize(list?.Find(x => x.name_JP == "燃えるゴミのコンテナ"), 20, 12);
+			FixContainerSize(list?.Find(x => x.name_JP == "燃えないゴミのコンテナ"), 20, 12);
+			FixContainerSize(list?.Find(x => x.name_JP == "肥料箱"), 20, 12);
+		}
+
+		private static void FixContainerSize(SourceThing.Row thing, int w, int h) {
+			var traits = thing?.trait;
+			if (traits == null) {
+				return;
+			}
+			for (int i = 0; i < traits.Count(); i++) {
+				try {
+					if (Regex.Match(traits[i], @"^(Container.*)|(.*Chest)|(Fridge)$").Success
+						&& (i + 2 < traits.Count())
+					) {
+						int wx = int.Parse(traits[i + 1]);
+						int hx = int.Parse(traits[i + 2]);
+						traits[i + 1] = (w).ToString();
+						traits[i + 2] = (h).ToString();
+						i += 2;
+					}
+
+				} catch (Exception) { }
+			}
+		}
 	}
 }
