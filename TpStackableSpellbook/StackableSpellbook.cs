b71efd0d7304458aba147d1108ed2923ff4de081
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:38:40 +0900

    TpStackableSpellbook  ElinBeta

--- TpStackableSpellbook/StackableSpellbook.cs
+++ TpStackableSpellbook/StackableSpellbook.cs
@@ -13,5 +13,27 @@ namespace TpStackableSpellbook
 	[HarmonyPatch]
 	public class StackableSpellbook
 	{
+		[HarmonyPrefix, HarmonyPatch(typeof(Card), nameof(Card.TryStackTo))]
+		public static bool TryStackTo(Card __instance, Thing to, ref bool __result) {
+			if (__instance.id == to.id && __instance.blessedState == to.blessedState) {
+				switch (__instance.id) {
+					case "spellbook":
+					case "372":
+						if (__instance.refVal != to.refVal) {
+							return true;
+						}
+						break;
+					default:
+						return true;
+				}
+
+				to.ModCharge(__instance.c_charges);
+				__instance.Destroy();
+				__result = true;
+
+				return false;
+			}
+			return true;
+		}
 	}
 }
