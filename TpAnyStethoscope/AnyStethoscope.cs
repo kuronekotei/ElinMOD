73dd6011b9203da8f99b246d11d906d1051a1a54
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:25:42 +0900

    TpAnyStethoscope  ElinBeta

---
+++ TpAnyStethoscope/AnyStethoscope.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using UnityEngine;
+
+namespace TpAnyStethoscope
+{
+	[HarmonyPatch]
+	public class AnyStethoscope
+	{
+		[HarmonyPrefix, HarmonyPatch(typeof(TraitStethoscope), nameof(TraitStethoscope.TrySetHeldAct))]
+		public static bool TrySetHeldAct(TraitStethoscope __instance, ActPlan p) {
+			p.pos.ListCards().ForEach((Action<Card>)(a =>
+			{
+				Chara c = a.Chara;
+				if (c == null || !p.IsSelfOrNeighbor || !EClass.pc.CanSee(a))
+					return;
+				p.TrySetAct("actInvestigate", (Func<bool>)(() =>
+				{
+					EClass.pc.Say("use_scope", (Card)c, __instance.owner);
+					EClass.pc.Say("use_scope2", (Card)c);
+					c.Talk("pervert2");
+					EClass.ui.AddLayer<LayerChara>().SetChara(c);
+					__instance.owner.ModCharge(-1);
+					if (__instance.owner.c_charges <= 0) {
+						EClass.pc.Say("spellbookCrumble", __instance.owner);
+						__instance.owner.Destroy();
+					}
+					return false;
+				}), (Card)c);
+			}));
+			return false;
+		}
+
+	}
+}
---
+++ TpAnyStethoscope/Mod_AnyStethoscope.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpAnyStethoscope
+{
+	[BepInPlugin("net.kuronekotei.any_stethoscope", "AnyStethoscope", "1.0.0.0")]
+	public class Mod_AnyStethoscope : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("AnyStethoscope").PatchAll();
+		}
+	}
+}
