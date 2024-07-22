48e35ff9b7fa5c75191e67611a54ce675e52efc1
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:37:08 +0900

    TpRemoveUnhealthyHobby  ElinBeta

--- TpRemoveUnhealthyHobby/RemoveUnhealthyHobby.cs
+++ TpRemoveUnhealthyHobby/RemoveUnhealthyHobby.cs
@@ -5,9 +5,6 @@ using System.Text;
 
 using BepInEx;
 using HarmonyLib;
-
-using ReflexCLI.Attributes;
-
 using UnityEngine;
 
 namespace TpHalloMod
@@ -15,5 +12,18 @@ namespace TpHalloMod
 	[HarmonyPatch]
 	public class RemoveUnhealthyHobby
 	{
+		[HarmonyPostfix, HarmonyPatch(typeof(Game), nameof(Game.OnBeforeInstantiate))]
+		public static void OnBeforeInstantiate() {
+			Debug.Log("RemoveUnhealthyHobby");
+			var list = EClass.sources.hobbies.listHobbies;
+			list.Remove(list.Find(x => x.name_JP == "気持ちいいこと"));
+			list.Remove(list.Find(x => x.name_JP == "飲酒"));
+			list.Remove(list.Find(x => x.name_JP == "喫煙"));
+			list.Remove(list.Find(x => x.name_JP == "薬"));
+			list.Remove(list.Find(x => x.name_JP == "自傷"));
+			list.Remove(list.Find(x => x.name_JP == "宝くじ"));
+			list.Remove(list.Find(x => x.name_JP == "ガチャ"));
+			list.Remove(list.Find(x => x.name_JP == "骨董品"));
+		}
 	}
 }
