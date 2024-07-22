faca09b83665b5cc3890d56e823865813243f985
TP@黒猫亭 <tp@kuronekotei.net>
2024-07-22 10:28:53 +0900

    TpHugeFridge  ElinBeta

--- TpHugeFridge/HugeFridge.cs
+++ TpHugeFridge/HugeFridge.cs
@@ -13,5 +13,34 @@ namespace TpHugeFridge
 	[HarmonyPatch]
 	public class HugeFridge
     {
+		[HarmonyPrefix, HarmonyPatch(typeof(Game), nameof(Game.OnGameInstantiated))]
+		public static void OnGameInstantiated_Prefix() {
+			List<SourceThing.Row> list = EClass.sources.things.rows;
+			var tgts = list?.FindAll(x => x.name_JP == "冷蔵庫");
+			foreach (var tgt in tgts) {
+				FixContainerSize(tgt, 12, 18);
+			}
+		}
+
+		private static void FixContainerSize(SourceThing.Row thing, int w, int h) {
+			var traits = thing?.trait;
+			if (traits == null) {
+				return;
+			}
+			for (int i = 0; i < traits.Count(); i++) {
+				try {
+					if (Regex.Match(traits[i], @"^(Container.*)|(.*Chest)|(Fridge)$").Success
+						&& (i + 2 < traits.Count())
+					) {
+						int wx = int.Parse(traits[i + 1]);
+						int hx = int.Parse(traits[i + 2]);
+						traits[i + 1] = (w).ToString();
+						traits[i + 2] = (h).ToString();
+						i += 2;
+					}
+
+				} catch (Exception) { }
+			}
+		}
 	}
 }
