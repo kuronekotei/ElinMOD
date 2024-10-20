a095cb364f0e77bf5c59fdbebd94b4a4b9aa733a
TP@黒猫亭 <tp@kuronekotei.net>
2024-10-20 20:09:19 +0900

    TpPerfectTrainer ElinBeta

---
+++ TpInfinityMagic/InfinityMagic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using UnityEngine;
+
+namespace TpInfinityMagic
+{
+	[HarmonyPatch]
+	public class InfinityMagic
+	{
+		static int vPot;
+		[HarmonyPrefix, HarmonyPatch(typeof(Chara), nameof(Chara.UseAbility), new Type[] { typeof(Act), typeof(Card), typeof(Point), typeof(bool) })]
+		public static void UseAbility_Prefix(Chara __instance, Act a) {
+			vPot=a.vPotential;
+			return;
+		}
+
+		[HarmonyPostfix, HarmonyPatch(typeof(Chara), nameof(Chara.UseAbility), new Type[] { typeof(Act), typeof(Card), typeof(Point), typeof(bool) })]
+		public static void UseAbility_Postfix(Chara __instance, Act a) {
+			if (a is Spell && __instance.IsPC) {
+				a.vPotential = vPot;
+				LayerAbility.SetDirty(a);
+			}
+
+			return;
+		}
+	}
+}
---
+++ TpInfinityMagic/Mod_InfinityMagic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+using UnityEngine;
+
+namespace TpInfinityMagic
+{
+	[BepInPlugin("net.kuronekotei.infinity_magic", "InfinityMagic", "1.0.0.0")]
+	public class Mod_InfinityMagic : BaseUnityPlugin
+	{
+		private void Start() {
+			new Harmony("InfinityMagic").PatchAll();
+		}
+	}
+}
