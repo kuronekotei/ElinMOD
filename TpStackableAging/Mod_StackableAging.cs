df873335d02cb4b1b651b1ff752647caa22aa277
TP@黒猫亭 <tp@kuronekotei.net>
2024-12-05 00:06:55 +0900

    TpStackableAging EA

---
+++ TpStackableAging/Mod_StackableAging.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+namespace TpStackableAging
+{
+	[BepInPlugin("net.kuronekotei.stackable_aging", "StackableAging", "1.0.0.0")]
+	public class Mod_StackableAging : BaseUnityPlugin
+	{
+		private void Awake() {
+			new Harmony("StackableAging").PatchAll();
+		}
+	}
+}
---
+++ TpStackableAging/StackableAging.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using UnityEngine;
+
+namespace TpStackableAging
+{
+	[HarmonyPatch]
+	public class StackableAging
+	{
+		static bool fStack;
+		[HarmonyPrefix, HarmonyPatch(typeof(TraitBrewery), nameof(TraitBrewery.OnChildDecay))]
+		public static void OnChildDecay(TraitBrewery __instance) {
+			fStack = true;
+		}
+		[HarmonyPostfix, HarmonyPatch(typeof(TraitBrewery), nameof(TraitBrewery.OnChildDecay))]
+		public static void OnChildDecayPostfix(TraitBrewery __instance) {
+			fStack = false;
+		}
+		[HarmonyPrefix, HarmonyPatch(typeof(Card), nameof(Card.AddThing), new Type[] { typeof(Thing), typeof(bool), typeof(int), typeof(int) })]
+		public static bool AddThing(Card __instance, Thing t, bool tryStack = true, int destInvX = -1, int destInvY = -1) {
+			if (fStack) {
+				fStack = false;
+				__instance.AddThing(t, true, destInvX, destInvY);
+				return false;
+			}
+			return true;
+		}
+	}
+}
